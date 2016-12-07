576f09116d76190a1c270797255924055647bb2f
Alen Štruklec <m3talen@gmail.com>
2016-12-07 08:59:47 +0100

    Add project files.

---
+++ src/TrecaDz/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrecaDz.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
